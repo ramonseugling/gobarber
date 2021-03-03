3af11499055363e5296ae964cb348f3dab22a55c
Ramon Seugling <ramonseugling@gmail.com>
2021-03-03 10:27:03 -0300

    first commit

---
+++ mobile/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3333',
+});
+
+export default api;
+
+/**
+ * iOS com Emulador: localhost
+ * iOS com físico: IP da máquina
+ * Android com Emulador: localhost (adb reverse)
+ * Android com Emulador: 10.0.2.2 (Android Studio)
+ * Android com físico: IP da máquina
+ */

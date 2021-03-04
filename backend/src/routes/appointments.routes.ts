74e45239767eea80676a759b05b3614eddd0e4be
Ramon Seugling <ramonseugling@gmail.com>
2021-03-04 07:29:05 -0300

    Created appointment repository

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,28 +1,42 @@
-import { request, response, Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns'; 
-import Appointment from '../models/Appointment';
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+// DTO - Data Transfer OBject
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments: Appointment[] = [];
+// SoC: Separation of Concerns (Separação de preocupações  )
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-    const { provider, date } = request.body;
-    
-    const parsedDate = startOfHour(parseISO(date));
-    
-    const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+  const { provider, date } = request.body;
 
-    if (findAppointmentInSameDate) {
-        return response.status(400).json({ message: 'This appointment is already booked!' });
-    }
+  const parsedDate = startOfHour(parseISO(date));
 
-    const appointment = new Appointment(provider, parsedDate);
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
 
-    appointments.push(appointment);
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked!' });
+  }
 
-    return response.json(appointment);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;

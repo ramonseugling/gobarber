80f602e616968a733eb2f674f6800832831e26f8
Ramon Seugling <ramonseugling@gmail.com>
2021-03-24 21:38:08 -0300

    Toast structure created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 import  { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => {
   return (
@@ -12,6 +13,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn/>
       </AuthProvider>
+
+      <ToastContainer />
 
       <GlobalStyle/>
     </>

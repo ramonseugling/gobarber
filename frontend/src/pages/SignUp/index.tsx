91c29620d4b984b70d2a2d478bec36995c77bb40
Ramon Seugling <ramonseugling@gmail.com>
2021-03-26 11:06:11 -0300

    Authenticate routes created

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -3,11 +3,13 @@ import { FiArrowLeft, FiMail, FiLock, FiUser} from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 import Input from'../../components/Input';
 import Button from'../../components/Button';
@@ -43,24 +45,26 @@ const SignUp: React.FC = () => {
       <Background/>
 
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+          <AnimationContainer>
+            <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome"/>
-          <Input name="email" icon={FiMail} placeholder="E-mail"/>
+              <Input name="name" icon={FiUser} placeholder="Nome"/>
+              <Input name="email" icon={FiMail} placeholder="E-mail"/>
 
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+              <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
 
-          <Button type="submit">Cadastrar</Button>
+              <Button type="submit">Cadastrar</Button>
 
-        </Form>
+            </Form>
 
-        <a href="signup">
-          <FiArrowLeft />
-          Voltar para logon
-        </a>
+            <Link to="/">
+              <FiArrowLeft />
+              Voltar para logon
+            </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   )

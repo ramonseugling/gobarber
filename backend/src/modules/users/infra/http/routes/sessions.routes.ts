0b6aad2822df60022fab0ca7e55f3b9aa9567917
Ramon Seugling <ramonseugling@gmail.com>
2021-03-31 13:02:30 -0300

    Dependency injection

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
@@ -7,16 +8,22 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = container.resolve(AuthenticateUserService);
 
   const { user, token } = await authenticateUser.execute({
     email,
     password,
   });
 
-  delete user.password;
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-  return response.json({ user, token });
+  return response.json({ user: userWithoutPassword, token });
 });
 
 export default sessionsRouter;
